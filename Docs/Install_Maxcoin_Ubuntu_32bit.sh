59f92047dbe5a409798e6ad76552e12be09abb9b
Oliver Morris <officialgamermorris@gmail.com>
2018-02-23 15:24:55 +0000

    Create Install_Maxcoin_Ubuntu_32bit.sh

---
+++ Docs/Install_Maxcoin_Ubuntu_32bit.sh
@@ -0,0 +1,39 @@
+# Maxcoin-Bash-Script
+Maxcoin Bash Script Ubuntu 16.04.3
+
+# Maxcoin Version 0.9.4.2
+
+
+![alt text](https://pbs.twimg.com/profile_images/878589713598251008/cs-cG7U6_400x400.jpg "Maxcoin Logo")
+
+## Step 1
+
+### Installing the Ubuntu 16.04.3 32 bit Version Bash script.
+
+```
+git clone https://github.com/Max-Coin/Maxcoin-Bash-Script/
+```
+
+## Step 2
+
+### Change Directory into Maxcoin-Bash-Script/
+
+```
+cd Maxcoin-Bash-Script/
+```
+
+## Step 3
+
+### Executing the Bash script
+
+```
+sudo chmod +x ./Installing_Maxcoin_Ubuntu_32bit.sh
+```
+
+## Step 4
+
+### Running the bash script
+
+```
+./Installing_Maxcoin_Ubuntu_32bit.sh
+```
